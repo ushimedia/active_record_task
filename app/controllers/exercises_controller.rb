fc4717b68da3592dd29b64ad9fedf8781d5be1af
ushioda <yusuke_ushioda@mediado.jp>
2022-04-25 12:18:11 +0900

    課題

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -2,13 +2,14 @@ class ExercisesController < ApplicationController
   def exercise1
     # 【要件】注文されていないすべての料理を返すこと
     #   * left_outer_joinsを使うこと
-    @foods = Food
+    @foods = Food.left_outer_joins(:order_foods).where(order_foods: { order_id: nil })
   end
 
   def exercise2
     # 【要件】注文されていない料理を提供しているすべてのお店を返すこと
     #   * left_outer_joinsを使うこと
     @shops = Shop
+
   end
 
   def exercise3 
@@ -16,6 +17,7 @@ class ExercisesController < ApplicationController
     #   * joinsを使うこと
     #   * 取得したAddressのインスタンスにorders_countと呼びかけると注文の数を返すこと
     @address = Address
+
   end
 
   def exercise4 
@@ -23,5 +25,6 @@ class ExercisesController < ApplicationController
     #   * joinsを使うこと
     #   * 取得したCustomerのインスタンスにfoods_price_sumと呼びかけると合計金額を返すこと
     @customer = Customer
+    
   end
 end
