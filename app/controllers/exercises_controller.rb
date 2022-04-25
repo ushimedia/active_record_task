b375f7d7eed4e7ff38be86793e738579fd3c3d73
ushioda <yusuke_ushioda@mediado.jp>
2022-04-25 12:48:11 +0900

    課題

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -8,8 +8,8 @@ class ExercisesController < ApplicationController
   def exercise2
     # 【要件】注文されていない料理を提供しているすべてのお店を返すこと
     #   * left_outer_joinsを使うこと
-    @shops = Shop
-
+    Food.left_outer_joins(:order_foods).where(order_foods: { order_id: nil })
+    @shops = Shop.left_outer_joins(:foods).where.not(foods: { food_id: nil })
   end
 
   def exercise3 
